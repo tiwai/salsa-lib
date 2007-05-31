f1ff3c55c101113f00945fcd580874ac77ffb829
Takashi Iwai <tiwai@suse.de>
2007-05-31 14:41:39 +0200

    autotoolized

--- Makefile
+++
@@ -1,8 +0,0 @@
-OBJS = control.o pcm.o pcm_params.o pcm_misc.o hcontrol.o mixer.o
-
-CFLAGS = -Wall -g -O2
-
-all: $(OBJS)
-
-clean:
-	rm -f $(OBJS)
---
+++ src/asoundlib.h
@@ -0,0 +1,26 @@
+#ifndef __ASOUNDLIB_H
+#define __ASOUNDLIB_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <assert.h>
+#include <endian.h>
+#include <sys/poll.h>
+#include <errno.h>
+#include <stdarg.h>
+
+#include "asoundef.h"
+/* #include "input.h" */
+#include "output.h"
+#include "error.h"
+#include "pcm.h"
+/* #include <alsa/rawmidi.h> */
+/* #include <alsa/timer.h> */
+/* #include <alsa/hwdep.h> */
+#include "control.h"
+#include "mixer.h"
+
+#endif /* __ASOUNDLIB_H */
