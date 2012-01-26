43dbf6594d4fc190c490442753bcf90880e47bfc
Takashi Iwai <tiwai@suse.de>
2012-01-26 11:05:15 +0100

    Update the copyright year in sources
    Not bad timing to say happy new year (for the old calendar)

--- src/asound.h
+++ src/asound.h
@@ -1,7 +1,7 @@
 /*
  * ALSA kernel API and common-type definitions
  *
- *  Copyright (c) 2011 by Takashi Iwai <tiwai@suse.de>
+ *  Copyright (c) 2011-2012 by Takashi Iwai <tiwai@suse.de>
  *
  *   This library is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Lesser General Public License as
--- src/input.h
+++ src/input.h
@@ -1,7 +1,7 @@
 /*
  *  SALSA-Lib - Input Macros
  *
- *  Copyright (c) 2007-2011 by Takashi Iwai <tiwai@suse.de>
+ *  Copyright (c) 2007-2012 by Takashi Iwai <tiwai@suse.de>
  *
  *   This library is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU Lesser General Public License as
