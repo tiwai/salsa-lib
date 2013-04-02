2d2439017a600b36ee554027586c2eb9b2c28f31
Mihail Zenkov <mihail.zenkov@gmail.com>
2013-04-02 11:59:20 +0000

    Fix error on jack compilation with salsa
    Fix error on jack compilation with salsa:
     /usr/include/alsa/ctl_macros.h:251:48: error: invalid conversion from
     'void*' to 'snd_ctl_elem_id_t* {aka snd_ctl_elem_id*}' [-fpermissive]
    
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- src/ctl_macros.h
+++ src/ctl_macros.h
@@ -248,7 +248,7 @@ int snd_ctl_elem_list_alloc_space(snd_ctl_elem_list_t *obj,
 				  unsigned int entries)
 {
 	free(obj->pids);
-	obj->pids = calloc(entries, sizeof(*obj->pids));
+	obj->pids = (snd_ctl_elem_id_t*)calloc(entries, sizeof(*obj->pids));
 	if (!obj->pids) {
 		obj->space = 0;
 		return -ENOMEM;
