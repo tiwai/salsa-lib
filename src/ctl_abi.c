1d0b0177421b42da1e6bfc6e36fb2a788b144f81
Takashi Iwai <tiwai@suse.de>
2015-06-01 16:25:40 +0200

    Add missing ABI-compatible snd_ctl_elem_*_name()
    Signed-off-by: Takashi Iwai <tiwai@suse.de>

--- src/ctl_abi.c
+++ src/ctl_abi.c
@@ -8,3 +8,21 @@
 #undef __SALSA_EXPORT_FUNC
 #define __SALSA_EXPORT_FUNC
 #include "ctl_macros.h"
+
+#undef snd_ctl_elem_type_name
+const char *snd_ctl_elem_type_name(snd_ctl_elem_type_t type)
+{
+	return _snd_ctl_elem_type_names[type];
+}
+
+#undef snd_ctl_elem_iface_name
+const char *snd_ctl_elem_iface_name(snd_ctl_elem_iface_t iface)
+{
+	return _snd_ctl_elem_iface_names[iface];
+}
+
+#undef snd_ctl_event_type_name
+const char *snd_ctl_event_type_name(snd_ctl_event_type_t type)
+{
+	return _snd_ctl_event_type_names[type];
+}
